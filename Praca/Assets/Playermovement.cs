24ce2e950472f22114983b6e1646124fbae8fd26
Skybowpl <szymus456@gmail.com>
2022-12-31 16:44:03 +0100

    Player movement
    Moveing, jumping, colissions

---
+++ Praca/Assets/Playermovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    private float horizontal;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHigh;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    void Update()
+    {
+        Debug.Log(IsGrounded());
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHigh);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapBox(groundCheck.position, new Vector2(1f, 0.1f), 0f, groundLayer);
+    }
+}
